47921e2d30974b47dc65eef55457b1c0c3415952
Diogo Martins <diogo.felix.martins@gmail.com>
2021-11-07 19:24:16 +0000

    Add zsh configs, plugins and theme

--- post-install.sh
+++ post-install.sh
@@ -1,4 +1,3 @@
-cd ~/Downloads
 sudo pacman -Syu --noconfirm
 
 localectl set-keymap --no-convert pt-latin1
@@ -19,35 +18,22 @@ sudo pacman -S --noconfirm yakuake
 # Add to startup ###################################
 mkdir ~/.config/autostart
 cp /usr/share/applications/org.kde.yakuake.desktop ~/.config/autostart
-# Alt+T keyboard shortcut
-echo >> ~/.config/kglobalshortcutsrc # empty line
-echo [yakuake] >> ~/.config/kglobalshortcutsrc
-echo _k_friendly_name=Yakuake >> ~/.config/kglobalshortcutsrc
-echo toggle-window-state=Alt+T,Alt+T,Open/Retract Yakuake >> ~/.config/kglobalshortcutsrc
-# Close on lose focus ##############################
-echo [Dialogs] >>  ~/.config/yakuakerc
-echo FirstRun=false >>  ~/.config/yrakuakerc
-echo  >>  ~/.config/yakuakerc  # empty line
-echo [Window] >>  ~/.config/yakuakerc
-echo KeepOpen=false >> ~/.config/yakuakerc
-# Apply theme (Installed materia-kde package) ######
-echo  >>  ~/.config/yakuakerc
-echo  [Appearance] >>  ~/.config/yakuakerc
-echo  Skin=materia-dark >>  ~/.config/yakuakerc
+# Copy config file
+cp configs/terminal/yakuakerc ~/.config
 
-sudo pacman -S --noconfirm nvidia nvidia-utils discord zsh
-
-### zsh (terminal)
-## Configs
+## zsh
 # Install configurations manager
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-Add plugins
+# Download plugins
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting)/' ~/.zshrc
-# Change theme
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
+# # Copy config files
+cp configs/terminal/agnoster.zsh-theme ~/.oh-my-zsh/themes
+cp configs/terminal/.zshrc ~/
 
-
+sudo pacman -S --noconfirm nvidia nvidia-utils discord zsh
 
 ####################################################
 ####################### YAY ########################
@@ -60,6 +46,3 @@ makepkg --noconfirm -si
 sudo rm -R yay-git
 
 yay -S --noconfirm google-chrome
-
-
-echo source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh >> ~/.zshrc

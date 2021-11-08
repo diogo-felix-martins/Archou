367b68da5e4cb62753fc23d3369a1c4b404cd673
Diogo Martins <diogo.felix.martins@gmail.com>
2021-11-08 19:06:27 +0000

    Disable kdewallet and add silent startup to steam
    Code refactor

--- post-install.sh
+++ post-install.sh
@@ -7,8 +7,11 @@ sudo touch /etc/sddm.conf.d/autologin.conf
 sudo bash -c 'sudo echo "[Autologin]" >> /etc/sddm.conf.d/autologin.conf'
 sudo bash -c 'sudo echo "User=diogom" >> /etc/sddm.conf.d/autologin.conf'
 sudo bash -c 'sudo echo "Session=plasma" >> /etc/sddm.conf.d/autologin.conf'
-## Auto unlock KDE Wallet
-sudo pacman -S --noconfirm kwallet-pam
+
+## Disable KDE Wallet
+sudo touch ~/.config/kwalletrc # TODO: test if file needs to be created on fresh install
+sudo bash -c 'sudo echo "[Wallet]" >> ~/.config/kwalletrc'
+sudo bash -c 'sudo echo "Enabled=false" >> ~/.config/kwalletrc'
 
 # Keyboard layout
 localectl set-keymap --no-convert pt-latin1
@@ -16,49 +19,25 @@ cp /etc/X11/xinit/xinitrc .xinitrc # Check if this does anything
 sudo echo setxkbmap -layout pt >> /etc/X11/xinit/xinitrc # Check if this does anything
 sed -i 's/\"us\"/\"pt\"/g' /etc/X11/xorg.conf.d/00-keyboard.conf
 
-### KDE Theme
-sudo pacman -S --noconfirm papirus-# Enable pacman multilib
-sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-
-sudo pacman -Syyu --noconfirmicon-theme
-sudo pacman -S --noconfirm materia-kde kvantum-theme-materia
-lookandfeeltool -a com.github.varlesh.materia-dark
-
 
 ####################################################
 ##################### PACKAGES #####################
 ####################################################
 
-### Yakuake ########################################
-sudo pacman -S --noconfirm yakuake zsh
-## Configs #########################################
-# Copy config file
-cp configs/terminal/yakuakerc ~/.config
+### Yakuake installation + configuration ###########
+sh ./yakuake.sh
 
-## zsh
-# Install configurations manager
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# Download plugins
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# # Copy config files
-cp configs/terminal/agnoster.zsh-theme ~/.oh-my-zsh/themes
-cp configs/terminal/MateriaDark.colorscheme ~/.local/share/konsole
-cp configs/terminal/zsh.profile ~/.local/share/konsole
-cp configs/terminal/.zshrc ~/
+### General packages ###############################
+sudo pacman -S --noconfirm nvidia nvidia-utils nvidia-settings discord ntfs-3g git
 
-sudo pacman -S --noconfirm nvidia nvidia-utils nvidia-settings discord ttf-liberation wqy-zenhei ntfs-3g
 
 ####################################################
 ####################### YAY ########################
 ####################################################
-
-sudo pacman -S --noconfirm git
 git clone https://aur.archlinux.org/yay-git
 cd yay-git
 makepkg --noconfirm -si
+cd ..
 sudo rm -R yay-git
 
 ### General packages ###############################
@@ -68,13 +47,14 @@ yay -S --noconfirm runelite google-chrome
 ## Enable pacman multilib
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 sudo pacman -Syyu --noconfirm
-
 yay -S --noconfirm lib32-fontconfig
-sudo pacman -S --noconfirm lib32-nvidia-utils
+sudo pacman -S --noconfirm lib32-nvidia-utils ttf-liberation wqy-zenhei
 sudo pacman -S --noconfirm steam
+
 
 ### Add applications to startup ####################
 mkdir ~/.config/autostart
 cp /usr/share/applications/org.kde.yakuake.desktop ~/.config/autostart
 cp /usr/share/applications/discord.desktop ~/.config/autostart
 cp /usr/share/applications/steam.desktop ~/.config/autostart
+sed -i 's/Exec=\/usr\/bin\/steam-runtime %U/Exec=\/usr\/bin\/steam-runtime -silent %U/g' ~/.config/autostart/steam.desktop
